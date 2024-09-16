09757b401aa38fa8400a78ddf743b5565e41acb4
aliskadmitr <alisadmitr@bk.ru>
2024-09-16 20:26:10 +0300

    Getting a list of files

--- App.py
+++ App.py
@@ -3,9 +3,17 @@ from flask import Flask, request, render_template
 # creating app
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
+    if request.method=='POST':
+        text = ''
+        if 'files[]' in request.files:
+            files = request.files.getlist("files[]")
+            for file in files:
+                print(file)
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
