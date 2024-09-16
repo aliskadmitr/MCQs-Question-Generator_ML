ec7a216454ee222d5c66e78ba495c9c9a5a3a56c
aliskadmitr <alisadmitr@bk.ru>
2024-09-16 20:45:22 +0300

    create a function that processes pdf files

--- App.py
+++ App.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, render_template
-
+from PyPDF2 import PdfReader, PdfWriter
 # creating app
 app = Flask(__name__)
+
+
+def process_pdf(file):
+    text = ""
+    pdf_reader = PdfReader(file)
+    for page_num in range(len(pdf_reader.pages)):
+        page_text = pdf_reader.pages[page_num]
+        text+=page_text
+    return text
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -10,7 +19,15 @@ def index():
         if 'files[]' in request.files:
             files = request.files.getlist("files[]")
             for file in files:
-                print(file)
+                if file.filename.endswith('.pdf'):
+                    #process pdf files
+                    text += process_pdf(file)
+                else:
+                    #process text file
+                    text += file.read().decode('utf-8')
+
+    num_questions = int(request.form['num_questions'])
+    mcqs = qenerate_mcqs(text, num_questions=num_questions)
     return render_template('index.html')
 
 if __name__ == '__main__':
